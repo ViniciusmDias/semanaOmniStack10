c9f4568f4d8c392644ee17c7f4b025d3663414b8
Vinicius Monteiro Dias <vinicius-mdias@hotmail.com>
2020-01-15 14:34:07 -0300

    Create app base

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const routes = require('./routes')
+
+const app = express();
+
+//MongoDb (Não-relacional)
+mongoose.connect('mongodb+srv://omnistack10:omnistack@omnistack10-9dxnh.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+//.use válida para todas as rotas da aplicação
+app.use(express.json());
+app.use(routes)
+
+// GET, POST, PUT, DELETE 
+
+// Query Params: request.query  (filtros, ordenação, páginação) incorporados na URL 
+// Route Params: request.params identificar um recurso na alteração ou remoção
+// Body: request.body dados para criação ou alteração de um registro
+
+app.listen(3333);
